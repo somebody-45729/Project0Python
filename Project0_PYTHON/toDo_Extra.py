082c9b387e5d70dfc3896b8e8e7428ccd3feb406
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-22 09:44:59 -0500

    update

--- Project0_PYTHON/toDo_Extra.py
+++ Project0_PYTHON/toDo_Extra.py
@@ -1,9 +1,11 @@
 # Extra class/module handler here
 
 class toDo_Task():
-    def __init__(self, task, date, priority):
+    def __init__(self, task, month, day, year, priority):
         self._task = str(task)
-        self._date = str(date)
+        self._month = int(month)
+        self._day = int(day)
+        self._year = int(year)
         self._priority = str(priority)
 
 class userEntry(toDo_Task):
