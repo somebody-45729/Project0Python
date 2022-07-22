abed476be1a90ce8d1e4d1c1cdb49281d8350b64
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-22 09:22:42 -0500

    update

--- Project0_PYTHON/toDo_Main.py
+++ Project0_PYTHON/toDo_Main.py
@@ -99,9 +99,9 @@ def insert_Task() -> toDo_Task:
     while True:
         # Enter the date relevant to the task/event
         try:
-            month = int(input("\nENTER MONTH OF EVENT/TASK IN NUMERIC FORM:\n>>>"))
-            day = int(input("\nENTER DAY OF EVENT/TASK IN NUMERIC FORM:\n>>>"))
-            year = int(input("\nENTER YEAR OF EVENT/TASK IN NUMERIC FORM:\n>>>"))
+            month = str(input("\nENTER MONTH OF EVENT/TASK IN NUMERIC FORM:\n>>>"))
+            day = str(input("\nENTER DAY OF EVENT/TASK IN NUMERIC FORM:\n>>>"))
+            year = str(input("\nENTER YEAR OF EVENT/TASK IN NUMERIC FORM:\n>>>"))
             date = datetime.date(month, day, year)
             check = re.search("[a-zA-Z].+ ',' ", date)
 
