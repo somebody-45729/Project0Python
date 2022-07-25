c6f8a8549ffc738ce073994fed7b224d4b067725
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-25 18:43:02 -0500

    update

--- Project0_PYTHON/toDo_Main.py
+++ Project0_PYTHON/toDo_Main.py
@@ -57,7 +57,7 @@ def load_Task(csvName):
         for row in f:
             infoLine = row.split(',')
             if infoLine[0] == "user":
-                toDo = userEntry(infoLine[1], infoLine[2], infoLine[3], infoLine[4], infoLine[5])
+                toDo = userEntry(infoLine[1], int(infoLine[2]), int(infoLine[3]), int(infoLine[4]), infoLine[5])
             else:
                 toDo = None
 
