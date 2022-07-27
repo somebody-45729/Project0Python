2ba04d03f011c8c6f669c8cf16745bf2d2d54156
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-27 17:52:52 -0500

    update

--- Project0_PYTHON/toDo_Extra.py
+++ Project0_PYTHON/toDo_Extra.py
@@ -1,12 +1,16 @@
 # Extra class/module handler here
 
-class toDo_Task():
+from abc import ABC, abstractmethod
+
+class toDo_Task(ABC):
     def __init__(self, task, month, day, year, priority):
         self._task = str(task)
         self._month = int(month)
         self._day = int(day)
         self._year = int(year)
         self._priority = str(priority)
+
+        @abstractmethod
 
         def __str__(self):
             return "Task: " + self._task + ", Month: " + str(self._month) + ", Day: " + str(self._day) + ", Year: " + str(self._year) + ", Priority: " + self._priority
